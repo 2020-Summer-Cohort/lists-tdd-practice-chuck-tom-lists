650bffe7493b08774e5c1e425d85f7d6d34118a5
Tom <tomrambo95@yahoo.com>
2020-05-20 13:15:48 -0400

    list TDD

--- List_TDD_Practice.Tests/ListTests.cs
+++ List_TDD_Practice.Tests/ListTests.cs
@@ -14,7 +14,7 @@ namespace List_TDD_Practice.Tests
         public void Lists_Are_Empty_By_Default()
         {
             var list = new List<int>();
-
+            
             // Replace __FILL_ME_IN__ with the correct value.
 
             // Remember, the Assert.Equal() method takes 2 parameters
@@ -22,7 +22,7 @@ namespace List_TDD_Practice.Tests
 
             // In this case ___FILL_ME_IN__ is what you expect the value to be
             // and list.Count is the actual value provided by your program
-            Assert.Equal(__FILL_ME_IN__, list.Count);
+            Assert.Empty(list);
         }
 
         [Fact]
@@ -31,6 +31,7 @@ namespace List_TDD_Practice.Tests
             var list = new List<int>()
             {
                 // Add 3 items to the list here.
+                1,2,3
             };
 
             Assert.Equal(3, list.Count);
